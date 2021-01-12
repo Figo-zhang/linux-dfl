b029fd21e1aa27bcd39d4a8f621cc78d19d5f77e
Tianfei zhang <tianfei.zhang@intel.com>
2021-01-13 01:03:56 +0800

    mfd: intel-m10-bmc: add PMCI driver
    Adding a driver for the PMCI-base interface of Intel MAX10
    BMC controller.
    
    Signed-off-by: Tianfei zhang <tianfei.zhang@intel.com>

--- include/linux/mfd/intel-m10-bmc.h
+++ include/linux/mfd/intel-m10-bmc.h
@@ -13,7 +13,8 @@
 /* Supported MAX10 BMC types */
 enum m10bmc_type {
 	M10_N3000,
-	M10_D5005
+	M10_D5005,
+	M10_PMCI
 };
 
 #define M10BMC_LEGACY_SYS_BASE		0x300400
@@ -163,6 +164,9 @@ enum m10bmc_type {
 
 /* Address of 4KB inverted bit vector containing staging area FLASH count */
 #define STAGING_FLASH_COUNT	0x17ffb000
+
+#define PMCI_M10BMC_SYS_BASE 0x0
+#define PMCI_M10BMC_SYS_END  0xfff
 
 enum m10bmc_fw_state {
 	M10BMC_FW_STATE_NORMAL,
