a19867d401f70fe745fc52b8881a08beaba38c51
Tianfei zhang <tianfei.zhang@intel.com>
2021-01-22 18:49:51 +0800

    fpga: m10bmc-sec: add image loader for PMCI
    Adding image loader functions like bmc image and fpga image
    for PMCI-based FPGA Card.
    
    Signed-off-by: Tianfei zhang <tianfei.zhang@intel.com>

--- include/linux/mfd/intel-m10-bmc.h
+++ include/linux/mfd/intel-m10-bmc.h
@@ -187,10 +187,20 @@ enum m10bmc_type {
 #define FLASH_HOST_REQUEST BIT(5)
 
 #define PMCI_M10BMC_DOORBELL 0x1c0
+#define PMCI_DRBL_REBOOT_DISABLED BIT(1)
+
 #define PMCI_M10BMC_AUTH_RESULT 0x1c4
 
 #define M10_FLASH_INT_US       1
 #define M10_FLASH_TIMEOUT_US   10000
+
+#define PMCI_M10BMC_MAX10_RECONF 0xfc
+#define PMCI_MAX10_REBOOT_REQ BIT(0)
+#define PMCI_MAX10_REBOOT_PAGE BIT(1)
+
+#define PMCI_M10BMC_FPGA_RECONF 0xb8
+#define PMCI_FPGA_RECONF_PAGE  GENMASK(22, 20)
+#define PMCI_FPGA_RP_LOAD      BIT(23)
 
 #define M10_PMCI(m10bmc) ((m10bmc)->type == M10_PMCI)
 #define M10_N3000(m10bmc) ((m10bmc)->type == M10_N3000)
