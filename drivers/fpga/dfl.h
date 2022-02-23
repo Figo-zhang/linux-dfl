f8640e440ec2f08d40cadeeb6fa969eab29e3933
Tianfei zhang <tianfei.zhang@intel.com>
2022-02-23 23:09:15 +0800

    fpga: dfl: tracking port conntected with AFU
    Introducing flags in dfl_fpga_cdev to track extensions
    or new features discovered during DFL enumeration. It uses
    some lowest bits of flags to track the port status which
    the AFU was connected to port device or not. In legacy
    model, the AFU was connected to Port device, but in "multiple
    VFs per PR slot" model, the AFU or PR slot without connected
    to Port device directly.
    
    Signed-off-by: Tianfei zhang <tianfei.zhang@intel.com>

--- drivers/fpga/dfl.h
+++ drivers/fpga/dfl.h
@@ -461,6 +461,13 @@ int dfl_fpga_enum_info_add_irq(struct dfl_fpga_enum_info *info,
 			       unsigned int nr_irqs, int *irq_table);
 void dfl_fpga_enum_info_free(struct dfl_fpga_enum_info *info);
 
+/*
+ * The lowest bits used to track the port status which AFU was connected
+ * with Port device. One bit represent one port device.
+ */
+#define DFL_PORT_CONNECT_BITS  MAX_DFL_FPGA_PORT_NUM
+#define DFL_FEAT_PORT_CONNECT_MASK ((1UL << (DFL_PORT_CONNECT_BITS)) - 1)
+
 /**
  * struct dfl_fpga_cdev - container device of DFL based FPGA
  *
@@ -470,6 +477,7 @@ void dfl_fpga_enum_info_free(struct dfl_fpga_enum_info *info);
  * @lock: mutex lock to protect the port device list.
  * @port_dev_list: list of all port feature devices under this container device.
  * @released_port_num: released port number under this container device.
+ * @flags: extensions discovered during DFL enumeration.
  */
 struct dfl_fpga_cdev {
 	struct device *parent;
@@ -478,6 +486,7 @@ struct dfl_fpga_cdev {
 	struct mutex lock;
 	struct list_head port_dev_list;
 	int released_port_num;
+	u64 flags;
 };
 
 struct dfl_fpga_cdev *
