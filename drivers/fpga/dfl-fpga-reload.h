d5372e0da30e31aad5c7eaf731afd871fea83ded
Tianfei Zhang <tianfei.zhang@intel.com>
2022-09-19 02:54:13 -0400

    cleanup dfl-fpga-reload.c
    Signed-off-by: Tianfei Zhang <tianfei.zhang@intel.com>

--- drivers/fpga/dfl-fpga-reload.h
+++ drivers/fpga/dfl-fpga-reload.h
@@ -7,11 +7,19 @@
 struct dfl_fpga_reload;
 struct dfl_fpga_trigger;
 
+/**
+ * struct dfl_fpga_reload_ops - image reload specific operations
+ * @prepare: Required: Prepare image reload
+ */
 struct dfl_fpga_reload_ops {
 	int (*prepare)(struct dfl_fpga_reload *dfl_reload);
-	int (*remove)(struct dfl_fpga_reload *dfl_reload);
 };
 
+/**
+ * struct dfl_fpga_trigger_ops - image trigger specific operations
+ * @available_images: Required: available images for reload trigger
+ * @image_trigger: Required: trigger the image reload on BMC
+ */
 struct dfl_fpga_trigger_ops {
 	ssize_t (*available_images)(struct dfl_fpga_trigger *trigger, char *buf);
 	int (*image_trigger)(struct dfl_fpga_trigger *trigger, const char *buf);
@@ -34,13 +42,14 @@ struct dfl_fpga_reload {
 	void *priv;
 };
 
+/* Timeout (10s) for image reload */
+#define RELOAD_TIMEOUT_MS  (10 * 1000)
+
 struct dfl_fpga_reload *
-dfl_fpga_reload_dev_register(struct module *module,
-                const struct dfl_fpga_reload_ops *ops, void *priv);
+dfl_fpga_reload_dev_register(const struct dfl_fpga_reload_ops *ops, void *priv);
 void dfl_fpga_reload_dev_unregister(struct dfl_fpga_reload *dfl_reload);
 struct dfl_fpga_trigger *
-dfl_fpga_reload_trigger_register(struct module *module,
-                const struct dfl_fpga_trigger_ops *ops, void *priv);
+dfl_fpga_reload_trigger_register(const struct dfl_fpga_trigger_ops *ops, void *priv);
 void dfl_fpga_reload_trigger_unregister(struct dfl_fpga_trigger *trigger);
 
 #endif
