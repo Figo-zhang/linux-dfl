436472ed55f65017180bd0fcb85ab9c9e2f6ae14
Tianfei Zhang <tianfei.zhang@intel.com>
2022-10-12 04:49:29 -0400

    fpga: dfl: remove non-reserved devices
    This patch introducing a new concept of non-reserved devices and
    reserved devices on DFL bus. Reserved devices mean that devices
    under DFL bus will be reserved before we trigger the image reload,
    because those devices provide a communication link to BMC during
    the trigger, for example SPI/NIOS private feature device on Intel
    PAC N3000 card, security update device. On the contrary, other
    devices are non-reserved devices, which will be removed before
    trigger the image reload.
    
    After the image reload, all of reserved and non-reserved devices
    will be removed.
    
    Signed-off-by: Tianfei Zhang <tianfei.zhang@intel.com>

--- drivers/fpga/dfl-pci.c
+++ drivers/fpga/dfl-pci.c
@@ -43,6 +43,18 @@ struct cci_drvdata {
 
 static int dfl_reload_prepare(struct dfl_image_reload *dfl_reload)
 {
+	struct device *trigger_dev = dfl_reload->trigger.parent;
+	struct pci_dev *pcidev = dfl_reload->priv;
+	struct cci_drvdata *drvdata = pci_get_drvdata(pcidev);
+	struct dfl_fpga_cdev *cdev = drvdata->cdev;
+	struct platform_device *fme = to_platform_device(cdev->fme_dev);
+
+	/* remove all of non-reserved fme devices of PF0 */
+	dfl_reload_remove_non_reserved_devs(fme, trigger_dev);
+
+	/* remove all AFU devices of PF0 */
+	dfl_reload_remove_afus(cdev);
+
 	return 0;
 }
 
