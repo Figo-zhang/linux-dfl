873c4ab5ae3244ae81eff9abe08ad1c912a31165
Tianfei Zhang <tianfei.zhang@intel.com>
2022-12-09 21:15:35 +0800

    PCI: hotplug: add register/unregister function for BMC device
    This patch introducing new APIs to register/unregister a
    BMC device instance for fpgahp driver.
    
    The fpgahp_bmc_device data structure represents a FPGA BMC device
    which has some specific callbacks for FPGA hotplug operations.
    The first one is available_images, which will return a list of
    available images for FPGA. The second one is the image_load,
    which will provide a reload trigger operation to BMC via secure
    update driver for Intel PAC N3000 Card.
    
    Signed-off-by: Tianfei Zhang <tianfei.zhang@intel.com>

--- include/linux/fpga/fpgahp_manager.h
+++ include/linux/fpga/fpgahp_manager.h
@@ -11,6 +11,33 @@
 
 struct pci_dev;
 struct fpgahp_manager;
+struct fpgahp_bmc_device;
+
+/**
+ * struct fpgahp_bmc_ops - fpga hotplug BMC specific operations
+ * @available_images: Required: available images for fpgahp trigger
+ * @image_trigger: Required: trigger the image reload on BMC
+ */
+struct fpgahp_bmc_ops {
+	ssize_t (*available_images)(struct fpgahp_bmc_device *bmc, char *buf);
+	int (*image_trigger)(struct fpgahp_bmc_device *bmc, const char *buf,
+			     u32 *wait_time_msec);
+};
+
+/**
+ * struct fpgahp_bmc_device - represent a fpga hotplug BMC device
+ *
+ * @ops: ops of this fpgahp_bmc_device
+ * @priv: private data for fpgahp_bmc_device
+ * @device: device of BMC device
+ * @registered: register status
+ */
+struct fpgahp_bmc_device {
+	const struct fpgahp_bmc_ops *ops;
+	void *priv;
+	struct device *device;
+	bool registered;
+};
 
 /**
  * struct fpgahp_manager_ops - fpgahp manager specific operations
@@ -43,6 +70,7 @@ enum fpgahp_manager_states {
  * @ops: ops of this fpgahp_manager
  * @state: the status of fpgahp_manager
  * @name: name of the fpgahp_manager
+ * @bmc: fpgahp BMC device
  * @registered: register status
  */
 struct fpgahp_manager {
@@ -51,12 +79,22 @@ struct fpgahp_manager {
 	const struct fpgahp_manager_ops *ops;
 	enum fpgahp_manager_states state;
 	const char *name;
+	struct fpgahp_bmc_device bmc;
 	bool registered;
 };
+
+static inline struct fpgahp_manager *to_fpgahp_mgr(struct fpgahp_bmc_device *bmc)
+{
+	return container_of(bmc, struct fpgahp_manager, bmc);
+}
 
 struct fpgahp_manager *fpgahp_register(struct pci_dev *hotplug_bridge,
 				       const char *name, const struct fpgahp_manager_ops *ops,
 				       void *priv);
 void fpgahp_unregister(struct fpgahp_manager *mgr);
+
+struct fpgahp_bmc_device *fpgahp_bmc_device_register(const struct fpgahp_bmc_ops *ops,
+						     struct device *dev, void *priv);
+void fpgahp_bmc_device_unregister(struct fpgahp_bmc_device *bmc);
 
 #endif
